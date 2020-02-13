d5e4ee7013f8b6182d186ed0fa50466433c17ed3
Pat Palmerston <patpalmerston@gmail.com>
2020-02-12 16:52:23 -0800

    added inventory to player

--- src/player.py
+++ src/player.py
@@ -6,6 +6,16 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
 
     def __str__(self):
         return f"name: {self.name} \n Room: {self.current_room}"
+
+    def get_inventory(self):
+        return self.inventory
+
+    def get_item(self, item):
+        return self.inventory.append(item)
+
+    def throw_item(self, item):
+        return self.inventory.remove(item)
