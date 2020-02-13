b3d943d2ddbafbbbbb3163d51fb8c0a1b70a41a8
Pat Palmerston <patpalmerston@gmail.com>
2020-02-12 16:39:44 -0800

    added items to room

--- src/room.py
+++ src/room.py
@@ -10,6 +10,16 @@ class Room:
         self.e_to = e_to
         self.w_to = w_to
         self.s_to = s_to
+        self.items = []
 
     def __str__(self):
         return f"name: {self.name}"
+
+    def get_items(self):
+        return self.items
+
+    def add_item(self, item):
+        return self.items.append(item)
+
+    def delete_item(self, item):
+        return self.items.remove(item)
