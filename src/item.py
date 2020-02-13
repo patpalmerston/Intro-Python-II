c1d9bad55c5a2e2d85f042040bf4724758f7665b
Pat Palmerston <patpalmerston@gmail.com>
2020-02-12 16:49:35 -0800

    updated item class

--- src/item.py
+++ src/item.py
@@ -6,8 +6,11 @@ class Item:
         self.name = name
         self.description = description
 
-    def pickup_item(self, item):
-        return f"you picked up {self.item}"
+    def __str__(self):
+        return f"Name: {self.name} \n Description: {self.description}"
 
-    def drop_item(self, item):
-        return f"you dropped the {self.item}"
+    def pickup_item(self):
+        return f"you picked up {self.name}"
+
+    def drop_item(self):
+        return f"you dropped the {self.name}"
